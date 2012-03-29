3696d77fb81baa5dd254979ce55a9973c0133a28
Logan Meers <loganwmeers@gmail.com>
2012-03-29 03:40:26 -0700

    Initial commit. Opcodes have been stubbed.

---
+++ app.js
@@ -0,0 +1,20 @@
+
+/**
+ * Module dependencies.
+ * 
+ * If Redis server is hosted elsewhere update createClient accordingly!
+ *
+ */
+
+var cc = require('chocolatechip8');
+
+//cc.hello();
+
+var x = 10;
+console.log(x.toString(16));
+
+var c8 = new cc.Chip8Emulator();
+//var c8 = new cc.Chip8Emulator();
+c8.loadROM("roms/pong.ch8");
+
+//cc.fuck();
